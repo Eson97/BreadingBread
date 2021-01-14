dae2bb41ebf64f04dd6ba0696e2b612ea1fb0bda
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2021-01-13 20:36:49 -0600

    Format GetListStoresByPath

--- BreadingBread.Application/UseCases/Stores/Queries/GetListStores/GetListStoresHandler.cs
+++ BreadingBread.Application/UseCases/Stores/Queries/GetListStores/GetListStoresHandler.cs
@@ -1,11 +1,7 @@
-using BreadingBread.Application.Exceptions;
 using BreadingBread.Application.Interfaces;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -22,12 +18,15 @@ namespace BreadingBread.Application.UseCases.Stores.Queries.GetListStores
 
         public async Task<GetListStoresResponse> Handle(GetListStoresQuery request, CancellationToken cancellationToken)
         {
-            var entity = await db.Store.Where(el => !el.IsDeleted).Select(el => new StoreModel
-            {
-                Id = el.Id,
-                Name = el.Name
+            var entity = await db.Store
+                .Where(el => !el.IsDeleted)
+                .Select(el => new StoreModel
+                {
+                    Id = el.Id,
+                    Name = el.Name
 
-            }).OrderBy(el => el.Name).ToListAsync(cancellationToken);
+                }).OrderBy(el => el.Name)
+            .ToListAsync(cancellationToken);
 
             return new GetListStoresResponse { Stores = entity };
         }
