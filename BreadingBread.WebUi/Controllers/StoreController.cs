4ae20482f1bfd59b21bda966c582daa6bb481e83
José Alberto Espinoza Morelos <spartan73rmy12@gmail.com>
2021-01-13 21:31:36 -0600

    Add StoreController

---
+++ BreadingBread.WebUi/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using BreadingBread.Application.UseCases.Stores.Queries.GetListStoresByPath;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BreadingBread.WebUi.Controllers
+{
+    [ApiController]
+    public class StoreController : BaseController
+    {
+        public StoreController()
+        {
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<GetListStoresByPathResponse>> GetList(int idPath)
+        {
+            return Ok(await Mediator.Send(new GetListStoresByPathQuery { IdPath = idPath }));
+
+        }
+    }
+}
