b90b00d97c2dd2994170f5781ba209c315230948
Ramiro Esaú Ramírez Alonzo <73205322+Eson97@users.noreply.github.com>
2021-11-09 21:28:03 -0600

    added get active paths in UserPath

---
+++ BreadingBread.Application/UseCases/UserPath/Queries/GetActivePaths/GetActivePathsQuery.cs
@@ -0,0 +1,12 @@
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadingBread.Application.UseCases.UserPath.Queries.GetActivePaths
+{
+    public class GetActivePathsQuery : IRequest<GetActivePathsResponse>
+    {
+        
+    }
+}
---
+++ BreadingBread.Application/UseCases/UserPath/Queries/GetActivePaths/GetActivePathsResponse.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BreadingBread.Application.UseCases.UserPath.Queries.GetActivePaths
+{
+    public class GetActivePathsResponse
+    {
+        public IList<ActivePathsModel> ActivePaths { get; set; }
+
+    }
+    public class ActivePathsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
