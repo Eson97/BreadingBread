e6964f442dec0da6e4575bcc3eea6ac9a2afeb00
Ramiro Esaú Ramírez Alonzo <73205322+Eson97@users.noreply.github.com>
2021-11-30 17:05:10 -0600

    new user type

---
+++ BreadingBread.Application/Security/IOtherRequest.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace BreadingBread.Application.Security
+{
+    interface IOtherRequest<TRequest, TResponse> : IAuthenticatedRequest<TRequest, TResponse> where TRequest : IRequest<TResponse>
+    {
+    }
+}
